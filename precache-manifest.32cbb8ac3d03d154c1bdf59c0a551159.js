7a9273fdc646642db64b92b78823a8480bd192c0
Júlio Cesar Gomes <julio_gomes92@hotmail.com>
2020-08-14 02:52:29 -0300

    Updates

---
+++ precache-manifest.32cbb8ac3d03d154c1bdf59c0a551159.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2755d37686cdc34a33dda3a6902a4151",
+    "url": "/ProjetoX/index.html"
+  },
+  {
+    "revision": "330ca9f89325451e64de",
+    "url": "/ProjetoX/static/css/main.3a95a639.chunk.css"
+  },
+  {
+    "revision": "9cb4d097b45732ace5d4",
+    "url": "/ProjetoX/static/js/2.54bcf236.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ProjetoX/static/js/2.54bcf236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "330ca9f89325451e64de",
+    "url": "/ProjetoX/static/js/main.70f51c56.chunk.js"
+  },
+  {
+    "revision": "38e982d5e17c39e52d6e",
+    "url": "/ProjetoX/static/js/runtime-main.eaf8e484.js"
+  },
+  {
+    "revision": "7c99c274051576caf6078bfcc2160d97",
+    "url": "/ProjetoX/static/media/logo.7c99c274.svg"
+  }
+]);

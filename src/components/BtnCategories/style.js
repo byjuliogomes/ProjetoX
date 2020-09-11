81454597b422b14163aa8db1b4c4ef2a24a343ae
Júlio <julio_gomes92@hotmail.com>
2020-09-11 11:44:28 -0300

    create buildpack react for heroku

---
+++ src/components/BtnCategories/style.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const ContainerBtnCategories = styled.div`
+  width: 5rem;
+  height: 5.25rem;
+  margin-right: 1.25rem;
+  text-align: center;
+`;
+
+export const BackCategories = styled.div`
+    display:flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const CircleCategories = styled.div`
+  width: 3.125rem;
+  height: 3.125rem;
+  background-color: #ffdddd;
+  margin-bottom: 0.5rem;
+  border-radius: 50%;
+  display:flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+
+export const IconCategories = styled.img`
+  width:1.875rem;
+  height:1.875rem;
+  background-size: cover;
+`;
+export const NameCategories = styled.div`
+  font-size: 9pt;
+`;

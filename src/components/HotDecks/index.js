9702d342c2e1065ac040767a38f8202af0106a18
Júlio <julio_gomes92@hotmail.com>
2020-09-17 22:01:00 -0300

    styled components errors fixed

--- src/components/HotDecks/index.js
+++ src/components/HotDecks/index.js
@@ -5,9 +5,9 @@ import { BtnDeck } from "../BtnDeck";
 const ContainerHotDecks = styled.div`
   margin-bottom: 2.625rem;
 
-  @media(min-width: 800px){
+  @media (min-width: 800px) {
     margin-bottom: 2.625rem;
-   }
+  }
 `;
 
 const ContainerTitulos = styled.div`
@@ -18,11 +18,11 @@ const ContainerTitulos = styled.div`
   align-items: center;
   justify-content: space-between;
 
-   @media(min-width: 800px){
+  @media (min-width: 800px) {
     margin-left: 12%;
     margin-right: 12%;
     margin-top: 0.75rem;
-   }
+  }
 `;
 
 const Title = styled.h3`
@@ -46,47 +46,40 @@ const Grid = styled.div`
   grid-template-columns: repeat(auto-fit, minmax(7.0625rem, 1fr));
   margin: 0 5% 0 5%;
 
-
-
   //   height: 360px;
   //   margin-left: 1.5rem;
   // margin-right: 1.5rem;
   // display: flex;
   // flex-wrap: wrap;
 
-  @media(min-width: 800px){
+  @media (min-width: 800px) {
     height: 100%;
-  display: grid;
-  grid-gap: 1.5625rem 1.125rem;
-  grid-auto-rows: auto;
-  grid-template-columns: repeat(auto-fit, minmax(6.875rem, 1fr));
-  margin: 0 12% 0 12%;
+    display: grid;
+    grid-gap: 1.5625rem 1.125rem;
+    grid-auto-rows: auto;
+    grid-template-columns: repeat(auto-fit, minmax(6.875rem, 1fr));
+    margin: 0 12% 0 12%;
   }
 `;
 
 const ContainerBtnDeckLeft = styled.div`
-
-  @media(min-width: 800px){
-    
+  @media (min-width: 800px) {
   }
 `;
 
 const ContainerBtnDeckRight = styled.div`
-
-  @media(min-width: 800px){
-    
+  @media (min-width: 800px) {
   }
 `;
 
 const NameDeck = styled.div`
-  text-align: center;  
+  text-align: center;
   font-size: 10pt;
   margin-top: 1.125rem;
   max-width: 7.5rem;
 
-  @media(min-width: 800px){
+  @media (min-width: 800px) {
     font-size: 10pt;
-    
   }
 `;
 
--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,23 +1,13 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import * as S from "./style";
 
-const LogoStyled = styled.img`
-background: url("././assets/logo_mobile.svg") no-repeat;
-width: ${props => (props.login ? "7.5rem" : "12.5rem")};
-height: auto;
-
-@media(min-width: 800px) {
-    width: ${props => (props.login ? "7.5rem" : "12.5rem")};
-    height: auto;
-  }
-`;
 
  const Logo = (props) => {
     return (
         <div>
-            <LogoStyled src="https://svgshare.com/i/Nvy.svg" class="Logo-mobile" login={props.login} />
+            <S.LogoStyled src="https://svgshare.com/i/Nvy.svg" class="Logo-mobile" login={props.login} />
         </div>
     )
 }
 
-export default Logo
+export default Logo;
